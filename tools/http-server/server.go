03c882b6bf962065203af0a420116181d380462e
Adam Hooper <adam@adamhooper.com>
2018-01-18 11:01:24 -0500

    Add log messages on HTTP requests

--- tools/http-server/server.go
+++ tools/http-server/server.go
@@ -26,6 +26,13 @@ func (handler endpointHandler) ServeHTTP(w http.ResponseWriter, r *http.Request)
   w.Write(endpoint.GetBody())
 }
 
+func LogRequests(handler http.Handler) http.Handler {
+  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
+    handler.ServeHTTP(w, r)
+  })
+}
+
 func main() {
   bytes, err := ioutil.ReadFile("/website-data.in-memory-website")
   if err != nil && os.IsNotExist(err) {
@@ -50,7 +57,7 @@ func main() {
   }
 
   log.Println("Listening on port 80")
-  err = http.ListenAndServe(":80", nil)
+  err = http.ListenAndServe(":80", LogRequests(http.DefaultServeMux))
   if err != nil {
     log.Fatal("ListenAndServe: ", err)
   }
